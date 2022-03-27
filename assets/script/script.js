68814d1e05ca02d8da6c4f3ffcf033b44413f476
JuanEstuniga <JuanCZuniga@juans-macbook.lan>
2022-03-27 13:26:25 -0400

    varaiables

---
+++ assets/script/script.js
@@ -0,0 +1,28 @@
+/* ------- Global Variable Declarations ------- */
+let quiz = document.querySelector("#quiz");
+let intro = document.querySelector("#introduction");
+let assesFT = document.querySelector("#assess-ft");
+let progressBar = document.querySelector(".progress");
+let startBtn = document.querySelector("#startBtn");
+let timeSpan = document.querySelector("#timeSpan");
+let questionH5 = document.querySelector("#question");
+let answersDiv = document.querySelector("#answers");
+let allDone = document.querySelector("#allDone");
+let finalScore = document.querySelector("#finalScore");
+let submit = document.querySelector("#submit");
+let highScoresList = document.querySelector("#highScoresList");
+let initials = document.querySelector("#initials");
+let clearHighscoresBtn = document.querySelector("#clearHighscoresBtn");
+let image_area = document.querySelector("#image_area");
+let totalSeconds = 60;
+let timeRemining = totalSeconds;
+let secondsElapsed = 0;
+let discountSeconds = 0;
+let currentQuestion = 0;
+let progress = 0;
+let correctAnswers = 0;
+let correctScore = 0;
+var localHighscoresArray = [];
+let time = setInterval(timer, 1000);
+let justRegistered = false;
+clearInterval(time);
