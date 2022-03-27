6f81b1da17f0082d47cc24127567d795265c6a90
JuanEstuniga <JuanCZuniga@juans-macbook.lan>
2022-03-27 16:44:26 -0400

    add functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -105,8 +105,236 @@ $("#staticBackdrop").on("shown.bs.modal", function (e) {
 });
 $("#staticBackdrop").on("hidden.bs.modal", function (e) {
   if (justRegistered) {
-    init();
+    initate();
   }
 });
 
-init();
+initate();
+/* ------------- Functions ------------- */
+
+function initate() {
+    timeSpan.textContent = timeRemining;
+    quiz.style.display = "none";
+    allDone.style.display = "none";
+    assesFT.style.display = "none";
+    intro.style.display = "block";
+    startBtn.style.display = "block";
+    progressBar.style.display = "none";
+  
+    totalSeconds = 60;
+    timeRemining = totalSeconds;
+    secondsElapsed = 0;
+    discountSeconds = 0;
+    currentQuestion = 0;
+    progress = 0;
+    correctAnswers = 0;
+    correctScore = 0;
+    justRegistered = false;
+    timeSpan.textContent = timeRemining;
+  
+    if (localStorage.getItem("highscore")) {
+      localHighscoresArray = localStorage.getItem("highscore").split(",");
+    }
+    clearInterval(time);
+    updateProgress();
+  
+    allDone.firstElementChild.setAttribute("class", "alert alert-info mt-0 mb-0");
+    submit.setAttribute("class", "btn btn-info");
+    progressBar.firstElementChild.setAttribute(
+      "class",
+      "progress-bar bg-info progress-bar-striped progress-bar-animated"
+    );
+  }
+  
+  function startQuiz() {
+    intro.style.display = "none";
+    startBtn.style.display = "none";
+    quiz.style.display = "block";
+    time = setInterval(timer, 1000);
+    progressBar.style.display = "block";
+    showQuestion();
+  }
+  
+  function timer() {
+    timeRemining = totalSeconds - secondsElapsed - 1 - discountSeconds;
+    timeSpan.textContent = timeRemining;
+    secondsElapsed++;
+    if (timeRemining <= 0) {
+      clearInterval(time);
+      disableQuestions();
+      gameOver("time_out");
+    }
+  }
+  
+  function showQuestion() {
+    questionH5.textContent = quizArray[currentQuestion].question;
+    var optionsBtnsArray = [];
+    var indexArray = [];
+  
+    for (i = 0; i < quizArray[currentQuestion].options.length; i++) {
+      var questionBtn = document.createElement("button");
+      questionBtn.setAttribute("type", "button");
+      questionBtn.setAttribute(
+        "class",
+        "list-group-item list-group-item-action list-group-item-info mt-1 answerButton"
+      );
+      questionBtn.setAttribute("data-index", i);
+      if (i === 0) {
+        questionBtn.setAttribute("correct", "yes");
+      } else {
+        questionBtn.setAttribute("correct", "no");
+      }
+      questionBtn.textContent = quizArray[currentQuestion].options[i];
+      answersDiv.append(questionBtn);
+      indexArray.push(i);
+    }
+  
+    answersDiv.childNodes.forEach(function (child) {
+      var rndIndex = Math.floor(Math.random() * indexArray.length);
+      answersDiv.append(answersDiv.children[rndIndex]);
+      indexArray.splice(rndIndex, 1);
+    });
+  }
+  
+  function disableQuestions() {
+    let questionsAssed = document.querySelectorAll(".answerButton");
+    questionsAssed.forEach((element) => {
+      element.setAttribute(
+        "class",
+        "list-group-item list-group-item-action list-group-item-danger mt-1 answerButton disabled"
+      );
+      if (
+        parseInt(element.getAttribute("data-index")) ===
+        quizArray[currentQuestion].correct
+      ) {
+        element.setAttribute(
+          "class",
+          "list-group-item list-group-item-action list-group-item-success mt-1 answerButton disabled"
+        );
+      }
+    });
+  }
+  
+  function assesSelection(event) {
+    if (event.target.matches("button")) {
+      var index = parseInt(event.target.getAttribute("data-index"));
+      var timeInterval = 1000;
+      disableQuestions();
+      if (event.target.getAttribute("correct") === "yes") {
+        displayFTAlert(true);
+        correctAnswers++;
+      } else {
+        discountSeconds += 3;
+        clearInterval(time);
+        time = setInterval(timer, 1000);
+        displayFTAlert(false);
+      }
+      currentQuestion++;
+      updateProgress();
+  
+      if (currentQuestion === quizArray.length) {
+        timeInterval = 5000;
+        gameOver("questions_done");
+      } else {
+        setTimeout(removeQuestionsButtons, 1000);
+        setTimeout(showQuestion, 1001);
+      }
+  
+      setTimeout(function () {
+        assesFT.style.display = "none";
+      }, timeInterval);
+    }
+  }
+  
+  function updateProgress() {
+    progress = Math.floor((currentQuestion / quizArray.length) * 100);
+    var styleStr = String("width: " + progress + "%; height: 100%;");
+    progressBar.firstElementChild.setAttribute("style", styleStr);
+    progressBar.firstElementChild.textContent = progress + " %";
+    correctScore = Math.floor((correctAnswers / quizArray.length) * 100);
+  }
+  
+  function displayFTAlert(correct) {
+    if (correct) {
+      assesFT.setAttribute(
+        "class",
+        "alert alert-success mt-0 mb-0 pt-0 pb-0 text-center"
+      );
+      assesFT.innerHTML = "<strong>Correct</strong>";
+      assesFT.style.display = "block";
+    } else {
+      assesFT.setAttribute(
+        "class",
+        "alert alert-danger mt-0 mb-0 pt-0 pb-0 text-center"
+      );
+      assesFT.innerHTML =
+        "<strong>Incorrect. </strong> 3 secs. discounted. Keep trying!!";
+      assesFT.style.display = "block";
+      timeSpan.style.color = "red";
+      setTimeout(function () {
+        timeSpan.style.color = "black";
+      }, 1000);
+    }
+  }
+  
+  function removeQuestionsButtons() {
+    questionH5.textContent = "";
+    var child = answersDiv.lastElementChild;
+    while (child) {
+      answersDiv.removeChild(child);
+      child = answersDiv.lastElementChild;
+    }
+    while (image_area.hasChildNodes()) {
+      image_area.removeChild(image_area.childNodes[0]);
+    }
+  }
+  
+  function gameOver(cause) {
+    if (cause === "questions_done") {
+      console.log("QUESTIONS DONE");
+      setTimeout(() => {
+        assesFT.setAttribute(
+          "class",
+          "alert alert-dark mt-0 mb-0 pt-0 pb-0 text-center"
+        );
+        assesFT.innerHTML = "<strong>Quiz finished</strong> Good luck!";
+      }, 1500);
+      clearInterval(time);
+    } else if (cause === "time_out") {
+      console.log("TIME OUT");
+      disableQuestions();
+      setTimeout(() => {
+      }, 4000);
+      assesFT.setAttribute(
+        "class",
+        "alert alert-info mt-0 mb-0 pt-0 pb-0 text-center"
+      );
+      assesFT.innerHTML = "<strong>Time finished</strong> Good luck!";
+    } else {
+      return false;
+    }
+    assesFT.style.display = "block";
+    if (correctScore >= 70) {
+      setTimeout(() => {
+      }, 5000);
+    } else {
+      setTimeout(() => {
+        allDone.firstElementChild.setAttribute(
+          "class",
+          "alert alert-danger mt-0 mb-0"
+        );
+        progressBar.firstElementChild.setAttribute(
+          "class",
+          "progress-bar bg-danger progress-bar-striped progress-bar-animated"
+        );
+        submit.setAttribute("class", "btn btn-danger");
+      }, 5000);
+    }
+    setTimeout(function () {
+      finalScore.textContent = correctScore;
+      quiz.style.display = "none";
+      allDone.style.display = "block";
+      assesFT.style.display = "none";
+      removeQuestionsButtons();
+    }, 5000);
+  }
